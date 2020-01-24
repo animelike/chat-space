bf6905f6fec0e7a94cb66ac80eefa9c6d968caa9
animelike <masarumaozhong3@gmail.com>
2020-01-24 12:28:29 +0900

    自動更新機能の実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,8 @@
 $(function(){
-  function  buildHTML(message){
-    if (message.image) {
+  var buildHTML = function(message){
+    if (message.content && message.image) {
       var html = 
-        `<div class="message" >
+        `<div class="message" data-message-id = ${message.id}>
           <div class="message_log-message">
             <div class="message__log-message__upper-info">
               <div class="message__log-message__upper-info__talker">
@@ -21,9 +21,9 @@ $(function(){
           <img src=${message.image} >
         </div>`
       return html
-    } else {
+    } else if (message.content) {
       var html =
-        `<div class="message" >
+        `<div class="message" data-message-id = ${message.id}>
           <div class="message_log-message">
             <div class="message__log-message__upper-info">
               <div class="message__log-message__upper-info__talker">
@@ -40,8 +40,25 @@ $(function(){
             </p>
           </div>
         </div>`
-      return html
+    } else if (message.image){
+      var html = 
+        `<div class="message" data-message-id = ${message.id}>
+          <div class="message_log-message">
+            <div class="message__log-message__upper-info">
+              <div class="message__log-message__upper-info__talker">
+                ${message.user_name}
+              </div>
+              <div class="message__log-message__upper-info__date">
+                ${message.created_at}
+              </div>
+            </div>
+          </div>
+          <div class="message__lower-message">
+            <img src=${message.image} >
+          </div>
+        </div>`
     };
+    return html;
   }
 
   $('#new_message').on('submit', function(e){
@@ -60,11 +77,39 @@ $(function(){
       var html = buildHTML(data);
       $('.messages').append(html);
       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-      $('form')[0].reset();
-      $('.form__submit-btn').prop('disabled', false);
+      $("#new_message")[0].reset();
+      $(".form__submit").prop("disabled", false);
     })
     .fail(function(){
       alert("メッセージ送信に失敗しました");
     });
   });
+
+  var reloadMessages = function() {
+    last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+        $("#new_message")[0].reset();
+        $(".form__submit").prop("disabled", false);
+      }
+    })
+    .fail(function() {
+      alert("メッセージの更新に失敗しました");
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
